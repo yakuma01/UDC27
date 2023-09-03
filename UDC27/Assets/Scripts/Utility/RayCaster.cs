92dde6743ee2d4c5ada7e8d591cadd37317bf7ee
abhmura <abhmura@iu.edu>
2023-09-03 02:42:36 -0400

    Removing prototypes

---
+++ UDC27/Assets/Scripts/Utility/RayCaster.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Utility
+{
+    public class RayCaster : MonoBehaviour
+    {
+        void FixedUpdate()
+        {
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+            {
+                Debug.Log(hit.transform.name);
+            }
+            
+        }
+    }
+}
